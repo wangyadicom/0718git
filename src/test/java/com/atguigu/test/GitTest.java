648e2426cbbca38ce73f08e1c5d48f45a247081d
wangyadicom <1400158068@qq.com>
2022-10-30 22:43:29 +0800

    zhangsan提交1.0

--- src/test/java/com/atguigu/test/GitTest.java
+++ src/test/java/com/atguigu/test/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println(2222);
         System.out.println("master");
         System.out.println("hotfix");
+        System.out.println("hello lisi");
     }
 }
